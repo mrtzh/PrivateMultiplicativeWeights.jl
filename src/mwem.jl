da0eb010f1af5ecaede7798a7bad3b54563a773b
mrtzh <m@mrtz.org>
2016-11-24 08:46:50 -0800

    new doc strings

--- src/mwem.jl
+++ src/mwem.jl
@@ -1,6 +1,8 @@
-#
-# Selects largest coordinate after noise addition
-#
+"""
+    noisy_max(mwstate::MWState)
+
+Select index of query with largest error after noise addition.
+"""
 function noisy_max(mwstate::MWState)
     diffs = mwstate.real_answers - evaluate(mwstate.queries, mwstate.synthetic)
     # do not select previously measured queries
@@ -9,15 +11,18 @@ function noisy_max(mwstate::MWState)
 end
 
 
-#
-# Perform multiplicative weights update
-#
+"""
+    update!(mwstate::MWState)
+
+Perform multiplicative weights update.
+"""
 function update!(mwstate::MWState)
     # repeatedly update queries that were already measured before
     for i = 1:mwstate.repetitions
         for qindex in keys(mwstate.measurements)
             query = get(mwstate.queries, qindex)
-            error = mwstate.measurements[qindex]-evaluate(query, mwstate.synthetic)
+            error = mwstate.measurements[qindex] 
+                        - evaluate(query, mwstate.synthetic)
             update!(query, mwstate.synthetic, error)
         end
     end
@@ -25,9 +30,20 @@ function update!(mwstate::MWState)
     mwstate
 end
 
-#
-# MWEM algorithm: Iterate noisy max + multiplicative weights update
-#
+"""
+    mwem
+
+Private Multiplicative Weights (MWEM) repeatedly selects largest error query
+and performs multiplicative weights update.
+
+# Arguments
+* `queries::Queries`: Set of queries
+* `data::Data`: Input data
+* `epsilon::Float=1.0`: Each iterations is `epsilon`-differentially private
+* `repetitions::Integer=10`: Repeatedly update with previously selected queries.
+* `verbose::Boolean=false`: Print output
+* `noisy_init::Boolean=false`: Initialize with noisy histogram if true.
+"""
 function mwem(queries::Queries, 
               data::Data;
               epsilon=1.0, 
@@ -46,7 +62,8 @@ function mwem(queries::Queries,
     for t = 1:iterations
         time = @elapsed begin
             i = noisy_max(mwstate)
-            mwstate.measurements[i] = mwstate.real_answers[i] + rand(Laplace(0.0, mwstate.scale))
+            mwstate.measurements[i] = mwstate.real_answers[i]
+                                          + rand(Laplace(0.0, mwstate.scale))
             update!(mwstate)
         end
         verbose ? print(t, "\t", maximum_error(mwstate), "\t", time, "\n") : nothing
