6098037b0e120b83a19262b82fc3d3a33f14b7a8
mrtzh <m@mrtz.org>
2016-11-23 23:26:56 -0800

    fixing include statement

--- examples/marginals.jl
+++ examples/marginals.jl
@@ -1,13 +1,13 @@
-using MWEM
+using PrivateMultiplicativeWeights
 
-function marginals(d=20,order=3,n=1000)
-    data_matrix = rand(0:1,d,n)
-    data_matrix[3,:] = data_matrix[1,:] .* data_matrix[2,:]
-    mwem(Parities(d,order),Tabular(data_matrix))
+function marginals(d=20, order=3, n=1000)
+    data_matrix = rand(0:1, d, n)
+    data_matrix[3, :] = data_matrix[1, :] .* data_matrix[2, :]
+    mwem(Parities(d, order), Tabular(data_matrix))
 end
 
-function factored_marginals(d=20,order=3,n=1000)
-    data_matrix = rand(0:1,d,n)
-    data_matrix[3,:] = data_matrix[1,:] .* data_matrix[2,:]
-    mwem(FactorParities(d,order),Tabular(data_matrix))
+function factored_marginals(d=20, order=3, n=1000)
+    data_matrix = rand(0:1, d, n)
+    data_matrix[3, :] = data_matrix[1, :] .* data_matrix[2, :]
+    mwem(FactorParities(d, order), Tabular(data_matrix))
 end
